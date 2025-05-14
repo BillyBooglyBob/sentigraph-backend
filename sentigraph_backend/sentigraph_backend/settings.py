cbd0558dd18f57c0e058fc41a7da1788623bc25f
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2025-05-14 23:36:49 +1000

    Deployment configurations

--- sentigraph_backend/sentigraph_backend/settings.py
+++ sentigraph_backend/sentigraph_backend/settings.py
@@ -23,7 +23,10 @@ AUTH_USER_MODEL = "useraccount.User"
 
 SITE_ID = 1
 
-WEBSITE_URL = "http://localhost:8000"
+if DEBUG:
+    WEBSITE_URL = "http://localhost:8000"
+else:
+    WEBSITE_URL = "http://209.38.95.111:1337"
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
@@ -56,6 +59,24 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://209.38.95.111",
+    "http://209.38.95.111:1337",
+]
+
+CORS_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://209.38.95.111",
+    "http://209.38.95.111:1337",
+]
+
+CORS_ORIGINS_WHITELIST = [
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://209.38.95.111",
+    "http://209.38.95.111:1337",
 ]
 
 # CORS_ALLOW_ALL_ORIGINS = True
